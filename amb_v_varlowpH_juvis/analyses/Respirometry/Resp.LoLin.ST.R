64d0cec637f9e89dee89da5d67a37d21ce285825
shelly trigg <shellywanamaker@gmail.com>
2019-09-30 17:09:01 -0700

    respirometry data analysis

--- amb_v_varlowpH_juvis/analyses/Respirometry/Resp.LoLin.ST.R
+++ amb_v_varlowpH_juvis/analyses/Respirometry/Resp.LoLin.ST.R
@@ -1,5 +1,3 @@
-
-
 ## load libraries
 library(readxl)
 library(devtools)
@@ -7,10 +5,68 @@ library(LoLinR)
 
 ## read in data
 
+data_paths <- "~/Documents/GitHub/P_generosa/amb_v_varlowpH_juvis/data/SDR"
+folder.list <- dir(data_paths)
+df_total <- data.frame()
+for(j in 1:length(folder.list)){
+  sub.folder.list <- dir(paste(data_paths, folder.list[j], sep = "/"))
+  runs_total <- data.frame()
+  for(i in 1:length(sub.folder.list)){
+    file.name <- dir(paste(data_paths, folder.list[j], sub.folder.list[i], sep = "/"), pattern = "OXY.xlsx", full.names = TRUE)
+    Resp.Data <- data.frame(read_xlsx(file.name, skip = 12)) # read in data
+    Resp.Data$Time_Min <- seq(0.25, (nrow(Resp.Data))*0.25, by=0.25)
+    Resp.Data$Date.Time <- as.POSIXct(strptime(Resp.Data$Date.Time, format ="%d.%m.%y %H:%M:%S"))
+    a <- 0.4
+    
+    resp_total <- data.frame() # create dataframe to save cumunalitively during for loop
+    resp.table <- data.frame(matrix(nrow = 1, ncol = 7)) # create dataframe to save each trial data during for loop
+    colnames(resp.table)<-c('Date', 'Run', 'SDR_position', 'Lpc', 'Leq' , 'Lz', 'alpha') # names for columns in the for loop
+    
+    for(k in 3:26){
+      model <- rankLocReg(
+        xall=Resp.Data$Time_Min, yall=Resp.Data[, k],
+        alpha=a, method="pc", verbose=TRUE) # run the LoLin script
+      sum.table<-summary(model)
+      resp.table$Date <- unique(format(as.POSIXct(Resp.Data$Date.Time,format="%m.%d.%y %H:%M:%S"),format="%m.%d.%y")) # all files have date in the form of yyyymmdd at the start of each csv name
+      resp.table$Run <- gsub("run","",sub.folder.list[i])
+      resp.table$SDR_position <- colnames(Resp.Data[k]) # assign the vial position - this will be related to contents (blank or individuals) later in script
+      resp.table$alpha <- a # set at start of script - reresents the proportion of data for final estimate of slopes (Lpc, Leq, Lz)
+      resp.table$Lpc <-sum.table$Lcompare[3,6] # Lpc slope 
+      resp.table$Leq <-sum.table$Lcompare[2,6] # Leq slope 
+      resp.table$Lz <-sum.table$Lcompare[1,6]  # Lz slope 
+      #resp.table$ci.Lz<-sum.table$Lcompare[1,9]
+      #resp.table$ci.Leq<-sum.table$Lcompare[2,9]
+      #resp.table$ci.Lpc<-sum.table$Lcompare[3,9]
+      # save plots every inside loop and name by date_run_vialposition
+      pdf(paste0("~/Documents/GitHub/P_generosa/amb_v_varlowpH_juvis/data/SDR/plots_alpha0.4/",resp.table$Date[1],"_",sub.folder.list[i],"_",colnames(Resp.Data[k]),"_regression.pdf"))
+      plot(model)
+      dev.off()
+      
+      resp_total <- rbind(resp_total,resp.table) #bind to a cumulative list dataframe
+    } # end of inside for loop
+    runs_total <- rbind(runs_total, resp_total)
+  }
+  df_total <- rbind(df_total, runs_total)
+  print(df_total[nrow(df_total),]) # print last row to monitor progress
+}
+
+ouputNAME<-"~/Documents/GitHub/P_generosa/amb_v_varlowpH_juvis/data/SDR/Cumulative_resp_alpha0.4.csv" 
+
+write.table(df_total,ouputNAME,sep=",", row.names=FALSE) # write out to the path names outputNAME
+
+
+
+
 Resp.Data <- data.frame(read_xlsx("~/Documents/GitHub/P_generosa/amb_v_varlowpH_juvis/data/SDR/20190905_SDR/round1/round1_710_OXY.xlsx", skip = 12))
+
+
+
+
 
 Resp.Data$Time_Min <- seq(0.25, (nrow(Resp.Data))*0.25, by=0.25)
 Resp.Data$Date.Time <- as.POSIXct(strptime(Resp.Data$Date.Time, format ="%m.%d.%y %H:%M:%S"))
+
+
 
 
 a <- 0.4
